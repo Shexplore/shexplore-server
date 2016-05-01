ad220d1bcc53dbc4e82d2653d567678a0d58903f
Hum4n01d <hum4n01d@icloud.com>
2016-05-01 16:47:09 -0700

    Fixed things

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,29 @@
-"use strict";
+'use strict';
 
 var gulp = require('gulp'),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-    stylus = require('gulp-stylus'),
-    maps = require('gulp-sourcemaps');
+    pug = require('gulp-pug'),
+    stylus = require('gulp-stylus');
 
-gulp.task("concatScripts", function() {
-    return gulp.src([
-        'public/js/main.js'
-        ])
-    .pipe(maps.init())
-    .pipe(concat('frontend.js'))
-    .pipe(maps.write('./'))
-    .pipe(gulp.dest('public/js'));
+gulp.task('compilePug', function() {
+  return gulp.src('**.jade')
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('./'));
 });
 
-gulp.task("minifyScripts", ["concatScripts"], function() {
-  return gulp.src("public/js/frontend.js")
-    .pipe(uglify())
-    .pipe(rename('frontend.min.js'))
-    .pipe(gulp.dest('public/js'));
+gulp.task('watchPug', function() {
+  gulp.watch('**.jade', ['compilePug']);
 });
 
-gulp.task('compileSass', function() {
-  return gulp.src("public/stylus/stylesheet.styl")
-      .pipe(maps.init())
-      .pipe(stylus())
-      .pipe(maps.write('./'))
-      .pipe(gulp.dest('public/css'));
+gulp.task('compileStylus', function() {
+  return gulp.src('**.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('watchStylus', function() {
-  gulp.watch('public/stylus/*.stylus', ['compileStylus']);
-})
+  gulp.watch('**.styl', ['compileStylus']);
+});
 
-gulp.task("build", ['minifyScripts', 'compileStylus']);
-
-gulp.task("default", ["build"]);
+gulp.task('default', ['compilePug', 'compileStylus', 'watchPug', 'watchStylus']);
